b741dcb9c11a51a181f43367ccf52291320be30d
Anton Prokhorov <betrayer11@gmail.com>
2017-02-25 22:54:45 +0700

    Extract `table_name` from functions

--- chat/handler.py
+++ chat/handler.py
@@ -5,12 +5,14 @@ import boto3
 
 client = boto3.client('dynamodb')
 
+table_name = 'chat_messages'
+
 
 def post(event, context):
     """Send message to the chat"""
 
     body = json.loads(event['body'])
-    client.put_item(TableName='messages',
+    client.put_item(TableName=table_name,
                     Item={
                         'message': {'S': body['message']},
                         'nick': {'S': body['nick']},
@@ -23,6 +25,6 @@ def post(event, context):
 def get(event, context):
     """Get all messages of the chat"""
 
-    data = client.scan(TableName='messages')
+    data = client.scan(TableName=table_name)
 
     return {'statusCode': 200, 'body': json.dumps(data)}
